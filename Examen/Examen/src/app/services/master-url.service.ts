a26ea4b5b4ddd99d5292940c57fb46b5b9d45c14
a <a>
2017-03-06 11:32:40 -0500

    url

--- Examen/Examen/src/app/services/master-url.service.ts
+++ Examen/Examen/src/app/services/master-url.service.ts
@@ -6,8 +6,8 @@ export class MasterURLService {
   private _url:string;
 
   constructor() {
-    //this._url = "http://localhost:1337";
-    this._url = "https://examen-twj-quishpedeidamia-deidy.c9users.io";
+    this._url = "http://localhost:1337";
+    //this._url = "https://examen-twj-quishpedeidamia-deidy.c9users.io";
   }
 
   get url():string{
